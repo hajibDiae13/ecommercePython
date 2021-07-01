bb45f9fc4a3fb36d9df62178ff64076a19babb5f
HAJIB Diae <diae.hajib@gmail.com>
2021-07-01 13:08:59 -0700

    Delete src directory

--- src/components/formButton/formButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './formButton.css'
-function FormButton(props) {
-  return (
-    <button class="FormButton">{props.content}</button>
-  )
-}
-
-export default FormButton
